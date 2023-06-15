4042895c79d9032f69aa85d639953ed82e0493b3
yetzabeth <yetzaher@gmail.com>
2023-06-15 08:39:20 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main-entry
+*
+*return: always 0 (success)
+*/
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+puts(string);
+return (0);
+}
