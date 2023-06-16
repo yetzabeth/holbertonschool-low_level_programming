651f47f415f67f79437386a10685388ec37894b0
yetzabeth <yetzaher@gmail.com>
2023-06-16 05:52:19 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main-entry
+*main-entriy
 *
 *Return: Always 0 (Success)
 */
@@ -8,41 +8,8 @@ int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int);)
-printf("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
