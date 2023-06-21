eb2cb0499230aa622c4267cfdc18a5fc7fd4eb52
yetzabeth <yetzaher@gmail.com>
2023-06-21 11:11:35 -0700

    tanks 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as
+ * and updates the value it points to to 98
+ * @n : integer
+ * Return : nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
