bc476ee826975eb621f74b2456fdcdfe7d6b3d65
yetzabeth <yetzaher@gmail.com>
2023-06-22 19:25:35 -0700

    tansk 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line
+ * @s : string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0') /* calculates length of a string */
+	{
+		i++;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
