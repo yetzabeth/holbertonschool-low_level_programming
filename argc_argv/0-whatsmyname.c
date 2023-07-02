1eef2c403d9c5dcdc207283d10e28fce44143730
yetzabeth <yetzaher@gmail.com>
2023-07-02 03:01:59 -0700

    mynewnameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main fuction
+ * @argc: saves the memory address
+ * @argv: pointer to pointer
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
