e2945d964be43c403e8dd15327f35b241771b9bb
yetzabeth <yetzaher@gmail.com>
2023-07-06 20:12:37 -0700

    tansk 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,14 +11,16 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, a, b = 0;
+	int i = 0;
+	int j = 0;
+	int a = 0;
+	int b = 0;
 	char *concat = NULL;
 
-	if (s1 == (NULL))
-		return ("");
-	if (s2 == (NULL))
-		return ("");
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	a = strlen(s1);
 	b = strlen(s2);
 	concat = malloc(a + b + 1);
