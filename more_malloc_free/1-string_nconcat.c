b9e9833ef3a46cbb6bbe10ff84a653387990e2ef
yetzabeth <yetzaher@gmail.com>
2023-07-09 04:35:33 -0700

    tansk 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,40 @@
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: first n bytes of s2
- * Return: pointer or NULLL
+ * string_nconcat - Concatenates two strings
+ * @s1 : string 1
+ * @s2 : string 2
+ * @n : number of chars of s2 to concatenate with s1
+ *
+ * Return: strings concatenated
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int size, i, j;
+	unsigned int i, j;
+	char *ar;
 
+	/* exceptions */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n > strlen(s2))
+	if (n >= strlen(s2))
 		n = strlen(s2);
 
-	size = strlen(s1) + n;
-	p = malloc((size + 6) * sizeof(char));
-	if (p == NULL)
-	{
-		free(p);
+
+	ar = malloc((strlen(s1) + n + 6) * sizeof(char)); /*considered the final \0*/
+	if (ar == NULL)
 		return (NULL);
-	}
 
 	for (i = 0; s1[i]; i++)
-		p[i] = s1[i];
+		ar[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
-		p[i] = s2[j];
-	p[i] = '\0';
+		ar[i] = s2[j];
 
-	return (p);
+	ar[i] = '\0';
+
+	return (ar);
 }
