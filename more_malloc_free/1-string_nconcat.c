271170c8a93406264b7ce1884b8ddb2475c25c1e
yetzabeth <yetzaher@gmail.com>
2023-07-09 04:42:38 -0700

    tansk 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = strlen(s2);
 
 
-	ar = malloc((strlen(s1) + n + 6) * sizeof(char)); /*considered the final \0*/
+	ar = malloc((strlen(s1) + n + 6 + 1) * sizeof(char)); /*considered the final \0*/
 	if (ar == NULL)
 		return (NULL);
 
