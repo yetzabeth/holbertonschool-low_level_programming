d0172563fc421d97a596a64699a77773fd785a93
yetzabeth <yetzaher@gmail.com>
2023-07-13 17:44:45 -0700

    tansk 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+/**
+ * main - main fuction
+ * @argc: saves the memory address
+ * @argv: pointer to pointer
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (((*argv[2] == '/') || (*argv[2] == '%')) && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if ((*(get_op_func(argv[2]))) && (strlen(argv[2]) == 1))
+	{
+	res = (*(get_op_func(argv[2])))(atoi(argv[1]), atoi(argv[3]));
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - returns the sum
+ * @a: argument
+ * @b: argument
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference
+ * @a: argument
+ * @b: argument
+ * Return: a + b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product
+ * @a: argument
+ * @b: argument
+ * Return: a + b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the div
+ * @a: argument
+ * @b: argument
+ * Return: a + b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the mod
+ * @a: argument
+ * @b: argument
+ * Return: a + b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
