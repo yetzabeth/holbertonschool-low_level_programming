b69c55e502ce420025cc81cbda2b9be70ac9612c
yetzabeth <yetzaher@gmail.com>
2023-07-26 09:39:11 -0700

    the number of elements

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Write a function that returns the number of elements
+ * @h: pointer
+ * Return: he number of elements in a linked list_t list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
